ac475d08d615311fd3c36355c165950b53ca11ba
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2023-02-08 12:37:16 +0100

    wip

--- connector/src/main/scala/com/celonis/kafka/connect/ems/sink/EmsSinkTask.scala
+++ connector/src/main/scala/com/celonis/kafka/connect/ems/sink/EmsSinkTask.scala
@@ -36,10 +36,9 @@ import com.celonis.kafka.connect.ems.storage.Writer
 import com.celonis.kafka.connect.ems.storage.WriterManager
 import com.celonis.kafka.connect.ems.utils.Version
 import com.celonis.kafka.connect.transform.SchemaInference
+import com.celonis.kafka.connect.transform.SchemaInference.ValueAndSchema
 import com.celonis.kafka.connect.transform.fields.FieldInserter
 import com.celonis.kafka.connect.transform.fields.PartitionOffset
-import com.celonis.kafka.connect.transform.SchemaInference.ValueAndSchema
-import com.celonis.kafka.connect.transform.SchemaInference.ValueAndSchema
 import com.celonis.kafka.connect.transform.flatten.Flattener
 import com.typesafe.scalalogging.StrictLogging
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
