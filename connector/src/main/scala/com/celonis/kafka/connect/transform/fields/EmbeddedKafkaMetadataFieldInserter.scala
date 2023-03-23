b083c86ca1df657d6c35e5c13094cf05bdeb69b4
Andrea Fiore <a.fiore@celonis.com>
2023-03-23 12:41:09 +0100

    bugfix: decouple kafka metadata fields from __celonisOrder (#27)

--- connector/src/main/scala/com/celonis/kafka/connect/transform/fields/EmbeddedKafkaMetadataFieldInserter.scala
+++ connector/src/main/scala/com/celonis/kafka/connect/transform/fields/EmbeddedKafkaMetadataFieldInserter.scala
@@ -4,31 +4,49 @@ import org.apache.kafka.connect.data.ConnectSchema
 import org.apache.kafka.connect.data.Field
 import org.apache.kafka.connect.data.Schema
 import org.apache.kafka.connect.data.Struct
+
 import scala.jdk.CollectionConverters._
 
-object EmbeddedKafkaMetadataFieldInserter extends FieldInserter {
+object EmbeddedKafkaMetadataFieldInserter {
   private val PartitionFieldName       = "kafkaPartition"
   private val OffsetFieldName          = "kafkaOffset"
   private val Timestamp                = "kafkaTimestamp"
   private val PartitionOffsetFieldName = "kafkaPartitionOffset"
 
-  //legacy field, kept for backward compatibility with previus connector versions
+  //legacy field, kept for backward compatibility with previous connector versions
   private[connect] val CelonisOrderFieldName = "__celonis_order"
+
+  private val PartitionOffsetTimestampFields = Seq(
+    PartitionFieldName       -> Schema.INT32_SCHEMA,
+    OffsetFieldName          -> Schema.INT64_SCHEMA,
+    Timestamp                -> Schema.INT64_SCHEMA,
+    PartitionOffsetFieldName -> Schema.STRING_SCHEMA,
+  )
+
+  private val CelonisOrderFields = Seq(
+    CelonisOrderFieldName -> Schema.INT64_SCHEMA,
+  )
+}
+class EmbeddedKafkaMetadataFieldInserter(fieldsToInsert: FieldsToInsert) extends FieldInserter {
+  import EmbeddedKafkaMetadataFieldInserter._
 
   override def insertFields(value: Any, meta: EmbeddedKafkaMetadata): Any =
     value match {
       case value: Struct =>
-        val s       = value.schema()
-        val fields  = s.fields().asScala.toList
-        val nextIdx = fields.map(_.index()).maxOption.map(_ + 1).getOrElse(0)
+        val s        = value.schema()
+        val fields   = s.fields().asScala.toList
+        val startIdx = fields.map(_.index()).maxOption.map(_ + 1).getOrElse(0)
 
-        val newFields = fields ++ List(
-          new Field(PartitionFieldName, nextIdx, Schema.INT32_SCHEMA),
-          new Field(OffsetFieldName, nextIdx + 1, Schema.INT64_SCHEMA),
-          new Field(Timestamp, nextIdx + 2, Schema.INT64_SCHEMA),
-          new Field(PartitionOffsetFieldName, nextIdx + 3, Schema.STRING_SCHEMA),
-          new Field(CelonisOrderFieldName, nextIdx + 4, Schema.INT64_SCHEMA),
-        )
+        val additionalFields = fieldsToInsert match {
+          case FieldsToInsert.PartitionOffsetTimestamp =>
+            PartitionOffsetTimestampFields
+          case FieldsToInsert.CelonisOrder =>
+            CelonisOrderFields
+          case FieldsToInsert.All =>
+            PartitionOffsetTimestampFields ++ CelonisOrderFields
+        }
+
+        val newFields = fields ++ fieldsWithIndex(startIdx)(additionalFields: _*)
 
         val newSchema = new ConnectSchema(s.`type`(),
                                           s.isOptional,
@@ -55,5 +73,10 @@ object EmbeddedKafkaMetadataFieldInserter extends FieldInserter {
         newValue
 
       case _ => value
+    }
+
+  private def fieldsWithIndex(startIdx: Int)(fieldSchemas: (String, Schema)*): Seq[Field] =
+    LazyList.from(startIdx).zip(fieldSchemas).map {
+      case (idx, (fieldName, schema)) => new Field(fieldName, idx, schema)
     }
 }
