e1b464aa646bc97e9dd1c459f4f1d643ac103724
Nicolò Martini <nicolo@martini.io>
2023-05-04 15:34:11 +0200

    Small optimisation in XmlConverter

--- connector/src/main/scala/com/celonis/kafka/connect/ems/converter/XmlConverter.scala
+++ connector/src/main/scala/com/celonis/kafka/connect/ems/converter/XmlConverter.scala
@@ -9,14 +9,15 @@ import org.apache.kafka.connect.storage.Converter
 import java.util
 
 final class XmlConverter extends Converter {
+  private val mapper = new XmlMapper()
+  private val typeReference = new TypeReference[Any]() {}
+
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = ()
 
   override def fromConnectData(topic: String, schema: Schema, value: Any): Array[Byte] =
     throw new NotImplementedError("XML Serialization has not been implemented")
 
   override def toConnectData(topic: String, value: Array[Byte]): SchemaAndValue = {
-    val mapper        = new XmlMapper()
-    val typeReference = new TypeReference[Any]() {}
     val node          = mapper.readValue(value, typeReference)
     new SchemaAndValue(null, node)
   }
