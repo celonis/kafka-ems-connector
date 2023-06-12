573c3906130cfb406a095e800fe7630cff1285bf
Nicolò Martini <nicolo@martini.io>
2023-06-12 11:23:58 +0200

    Add/update headers (#31)

--- connector/src/test/scala/com/celonis/kafka/connect/ems/storage/FileSystemOperationsTests.scala
+++ connector/src/test/scala/com/celonis/kafka/connect/ems/storage/FileSystemOperationsTests.scala
@@ -1,8 +1,8 @@
 /*
- * Copyright 2022 Celonis SE
+ * Copyright 2023 Celonis SE
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this path except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
